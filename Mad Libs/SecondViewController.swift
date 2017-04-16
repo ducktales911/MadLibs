0eec25afbccf8f6809d17a33573c1a93e4f3c69d
Thomas Hamburger <thomas@esigaretshop.nl>
2017-04-16 19:17:31 +0200

    Navigation controller toegevoegd, tweede view controller toegevoegd met textfield.

---
+++ Mad Libs/SecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  Mad Libs
+//
+//  Created by Thomas Hamburger on 16-04-17.
+//  Copyright © 2017 Thomas Hamburger. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
